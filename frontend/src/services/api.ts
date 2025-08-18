da4f0d908b151025231d89c4cb61fcc3e26ff219
mohamedaziz <hammaarfaoui@gmail.com>
2025-08-18 19:19:08 +0100

    testpreprod1.0

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const API_URL = process.env.REACT_APP_API_URL || 'https://agritrace.azizarfaoui.ip-ddns.com/api';
+const API_URL = 'https://agritrace.azizarfaoui.ip-ddns.com/api';
 
 // Configure axios with default headers
 axios.interceptors.request.use((config) => {
